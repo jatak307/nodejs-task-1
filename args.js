5d51a273979ef3d368a82c1f7b78553553337712
jatak307 <jatak307@gmail.com>
2021-11-09 01:34:33 +0300

    feat: implement config validation

--- args.js
+++ args.js
@@ -1,7 +1,12 @@
 const args = process.argv.reduce((acc, current, i, arr) => {
   if (current === "-c" || current === "--config") {
     isDuplicated(acc, "config");
-    acc["config"] = arr[i + 1];
+    if (isValidConfig(arr[i + 1])) {
+      acc["config"] = arr[i + 1];
+    } else {
+      process.stderr.write(`Error: The config is not valid.`);
+      process.exit(1);
+    }
   } else if (current === "-i" || current === "--input") {
     isDuplicated(acc, "input");
     acc["input"] = arr[i + 1];
@@ -16,6 +21,36 @@ function isDuplicated(obj, key) {
   if (obj[key]) {
     process.stderr.write(`Error: The process was aborted because some of the arguments are duplicated.`);
     process.exit(1);
+  }
+}
+
+function isValidConfig(config) {
+  let configArr;
+  if (config[config.length - 1] === "-") {
+    const configArr = config.slice(0, -1).split("-");
+    return configArr.every((elem) => isValidElem(elem));
+  }
+  configArr = config.split("-");
+  return configArr.every((elem) => isValidElem(elem));
+}
+
+function isValidElem(el) {
+  return el.length === 1
+    ? isValidAtbash(el)
+    : el.length === 2
+      ? isValidCaesar(el)
+      : false;
+}
+
+function isValidAtbash(el) {
+  return el === "A" ? true : false;
+}
+
+function isValidCaesar(el) {
+  if (el[0] === "C" || el[0] === "R") {
+    return Number(el[1]) <= 1 ? true : false;
+  } else {
+    return false;
   }
 }
 
--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const { pipeline } = require('stream');
 const readStream = require('./read');
 const writeStream = require('./write');
+const args = require('./args');
+
+console.log('INDEX', args);
 
 pipeline(readStream, writeStream, (err) => {
   if (err) {
