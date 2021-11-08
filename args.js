673a7f553e94648296daae55b6a5a43ddf1bb3f4
jatak307 <jatak307@gmail.com>
2021-11-09 01:38:57 +0300

    refactor: fix code formatting

--- args.js
+++ args.js
@@ -1,18 +1,18 @@
 const args = process.argv.reduce((acc, current, i, arr) => {
-  if (current === "-c" || current === "--config") {
-    isDuplicated(acc, "config");
+  if (current === '-c' || current === '--config') {
+    isDuplicated(acc, 'config');
     if (isValidConfig(arr[i + 1])) {
-      acc["config"] = arr[i + 1];
+      acc['config'] = arr[i + 1];
     } else {
       process.stderr.write(`Error: The config is not valid.`);
       process.exit(1);
     }
-  } else if (current === "-i" || current === "--input") {
-    isDuplicated(acc, "input");
-    acc["input"] = arr[i + 1];
-  } else if (current === "-o" || current === "--output") {
-    isDuplicated(acc, "output");
-    acc["output"] = arr[i + 1];
+  } else if (current === '-i' || current === '--input') {
+    isDuplicated(acc, 'input');
+    acc['input'] = arr[i + 1];
+  } else if (current === '-o' || current === '--output') {
+    isDuplicated(acc, 'output');
+    acc['output'] = arr[i + 1];
   }
   return acc;
 }, {});
@@ -26,11 +26,11 @@ function isDuplicated(obj, key) {
 
 function isValidConfig(config) {
   let configArr;
-  if (config[config.length - 1] === "-") {
-    const configArr = config.slice(0, -1).split("-");
+  if (config[config.length - 1] === '-') {
+    const configArr = config.slice(0, -1).split('-');
     return configArr.every((elem) => isValidElem(elem));
   }
-  configArr = config.split("-");
+  configArr = config.split('-');
   return configArr.every((elem) => isValidElem(elem));
 }
 
@@ -43,11 +43,11 @@ function isValidElem(el) {
 }
 
 function isValidAtbash(el) {
-  return el === "A" ? true : false;
+  return el === 'A' ? true : false;
 }
 
 function isValidCaesar(el) {
-  if (el[0] === "C" || el[0] === "R") {
+  if (el[0] === 'C' || el[0] === 'R') {
     return Number(el[1]) <= 1 ? true : false;
   } else {
     return false;
