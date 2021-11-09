ac65de17deed9e9f8dbdadf7553eefeaa65fadc3
jatak307 <jatak307@gmail.com>
2021-11-10 02:24:55 +0300

    feat: add a check for file existence to the read stream

--- read.js
+++ read.js
@@ -1,5 +1,21 @@
 const fs = require('fs');
+const path = require('path');
+const { input } = require('./args');
 
-const readStream = fs.createReadStream('./input.txt');
+let readStream;
+let readableFile = path.resolve(input);
+
+if (input) {
+  try {
+    fs.accessSync(readableFile, fs.constants.R_OK);
+    readStream = fs.createReadStream(readableFile);
+  } catch (err) {
+    process.stderr.write(`Error: No access!`);
+    process.exit(1);
+  }
+} else {
+  readStream = process.stdin;
+}
+
 
 module.exports = readStream;
